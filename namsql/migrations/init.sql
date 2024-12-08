452f3c3904fe8f86c5509580971fbea99ea3111d
dev-ijtech <dev@ijtech.co.uk>
2024-12-08 21:30:54 +0000

    Add the init script used to generate an empty DB for namd

--- namsql/migrations/init.sql
+++ namsql/migrations/init.sql
@@ -1,1 +1,18 @@
 /* it's pretty self-explanatory, run this script on an empty DB to initialise it to be used by nam */
+
+PRAGMA journal_mode = WAL;
+
+PRAGMA busy_timeout = 5000;
+
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE "Device" (
+  "id"             INTEGER,
+  "name"           TEXT NOT NULL,
+  "managementIPv4" TEXT NOT NULL,
+  "vendor"         TEXT NOT NULL,
+  "version"        TEXT NOT NULL,
+  "createdAt"      TEXT NOT NULL,
+  "updatedAt"      TEXT NOT NULL,
+  PRIMARY KEY ("id")
+) STRICT;
