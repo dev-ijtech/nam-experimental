12478835061d342698033384a517f851440e9049
dev-ijtech <dev@ijtech.co.uk>
2024-12-08 21:30:08 +0000

    Change Validator interface

--- valid.go
+++ valid.go
@@ -1,5 +1,30 @@
 package nam
 
+import "fmt"
+
 type Validator interface {
-	Valid() (problems map[string]string)
+	Valid() ProblemSet
+}
+
+type Problem struct {
+	Name   string
+	Reason string
+}
+
+type ProblemSet struct {
+	Set []Problem
+}
+
+func (p *ProblemSet) Add(name string, reason string) {
+	p.Set = append(p.Set, Problem{name, reason})
+}
+
+func (p *ProblemSet) String() string {
+	problemString := ""
+
+	for i, problem := range p.Set {
+		problemString += fmt.Sprintf("%d. %s %s\n", i+1, problem.Name, problem.Reason)
+	}
+
+	return problemString
 }
